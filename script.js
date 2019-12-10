d0ff71c68ea0102ac8a2de3fd77ae0dafc4d7f2c
Andrés Del Carpio <andresdc@andresdc.net>
2019-12-10 17:13:26 -0500

    Added basic display functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// VARIABLES
+const resultDisplay     = document.querySelector('.result');
+const operationsDisplay = document.querySelector('.operations');
+const numButtons        = document.querySelectorAll('#num');
+const opButtons         = document.querySelectorAll('#op');
+const clearButton       = document.querySelector('#clear');
+const backspaceButton   = document.querySelector('#backspace');
+const buttons           = document.querySelectorAll('.button');
+
+
+// MATH FUNCTIONS
 function add (x, y) {
   return x + y;
 }
@@ -22,3 +33,29 @@ function operate (operator, x, y) {
     case '/': return divide(x, y);
   }
 }
+
+
+// DISPLAY FUNCTIONS
+const buttonsArray = Array.from(numButtons).concat(Array.from(opButtons));
+
+buttonsArray.forEach(e => {
+    e.addEventListener('click', () => {
+      operationsDisplay.innerText === '0' ?
+        operationsDisplay.textContent = `${e.innerText}` :
+        operationsDisplay.textContent += `${e.innerText}`;
+    })
+});
+
+clearButton.addEventListener('click', () => {
+  operationsDisplay.textContent = '0';
+  resultDisplay.textContent = '0';
+});
+
+backspaceButton.addEventListener('click', () => {
+  const op = operationsDisplay.innerText;
+  operationsDisplay.textContent = op.slice(0, -1);
+});
+
+resultDisplay.addEventListener('click', () => {
+  resultDisplay.textContent = operationsDisplay.innerText;
+});
