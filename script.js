ea8a8d4968e6a602b8757e6a576cefa951933c85
Andrés Del Carpio <andresdc@andresdc.net>
2019-12-10 14:27:43 -0500

    Change function expressions to declarations

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const add = function (x, y) {
+function add(x, y) {
   return x + y;
 }
 
-const substract = function (x, y) {
+function substract(x, y) {
   return x - y;
 }
 
-const multiply = function (x, y) {
+function multiply(x, y) {
   return x * y;
 }
 
-const divide = function (x, y) {
+function divide(x, y) {
   return x / y;
 }
 
-const operate = function (operator, x, y) {
+function operate(operator, x, y) {
   switch (operator) {
     case '+': return add(x, y);
     case '-': return substract(x, y);
