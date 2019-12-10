94538b2d8e656d0d90c6cb56040e0bbae4769de9
Andrés Del Carpio <andresdc@andresdc.net>
2019-12-10 17:12:58 -0500

    Fixed js style

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function add(x, y) {
+function add (x, y) {
   return x + y;
 }
 
-function substract(x, y) {
+function substract (x, y) {
   return x - y;
 }
 
-function multiply(x, y) {
+function multiply (x, y) {
   return x * y;
 }
 
-function divide(x, y) {
+function divide (x, y) {
   return x / y;
 }
 
-function operate(operator, x, y) {
+function operate (operator, x, y) {
   switch (operator) {
     case '+': return add(x, y);
     case '-': return substract(x, y);
